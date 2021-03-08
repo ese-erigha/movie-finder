ec0dbb233e6d04177970e77b56217c53474d1c97
Eseoghene Erigha <ese.erigha@gmail.com>
2021-03-08 16:44:42 +0100

    add more styling to background and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="footer text-center mt-4 mb-4">
+  <footer className="footer text-center mt-4 mb-4 white">
     &#169; 2021 made with
     <i className="fa fa-heart heart space-left-5" aria-hidden="true" />
     <span className="space-left-5">by</span>
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -3,13 +3,15 @@ import Card from 'react-bootstrap/Card';
 
 const MovieCard = () => {
   console.log('Hello');
+  // /95S6PinQIvVe4uJAd82a2iGZ0rA.jpg
+  // /6KErczPBROQty7QoIsaa6wJYXZi.jpg
   return (
     <Card className="movie-card">
       <a href="https://api-cinema-10d15.firebaseapp.com/movie/587807">
         <Card.Img
           className="fadeIn animated"
           variant="top"
-          src="https://image.tmdb.org/t/p/w300/6KErczPBROQty7QoIsaa6wJYXZi.jpg"
+          src="https://image.tmdb.org/t/p/w300/95S6PinQIvVe4uJAd82a2iGZ0rA.jpg"
         />
         <Card.Body>
           <span className="card-rating text-center">7.4</span>
