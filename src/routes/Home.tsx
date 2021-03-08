8fad730f1e088df8a1f596b48b31ce9c058a0deb
Eseoghene Erigha <ese.erigha@gmail.com>
2021-03-08 12:48:37 +0100

    Added react-helmet

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import MovieCard from 'components/MovieCard';
 
 const Home = () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   return (
-    <div className="d-flex flex-wrap justify-content-md-between justify-content-center">
-      {arr.map((item) => (
-        <MovieCard key={item} />
-      ))}
-    </div>
+    <>
+      <Helmet>
+        <title>MovieX</title>
+      </Helmet>
+      <div className="d-flex flex-wrap justify-content-md-between justify-content-center">
+        {arr.map((item) => (
+          <MovieCard key={item} />
+        ))}
+      </div>
+    </>
   );
 };
 export default Home;
