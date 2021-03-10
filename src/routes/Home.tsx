31665b324c0b2ec20546c4a95b2613061548c02c
Eseoghene Erigha <ese.erigha@gmail.com>
2021-03-10 03:11:06 +0100

    Added pagination with css

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ReactPaginate from 'react-paginate';
 import { Helmet } from 'react-helmet';
 import MovieCard from 'components/MovieCard';
 
 const Home = () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const props = {
+    pageCount: 200,
+    initialPage: 1,
+  };
+
+  const handlePageClick = (data: { selected: number }) => {
+    console.log(data);
+  };
+
   return (
     <>
       <Helmet>
@@ -14,6 +24,21 @@ const Home = () => {
           <MovieCard key={item} />
         ))}
       </div>
+      <ReactPaginate
+        previousLabel="&larr;"
+        nextLabel="&rarr;"
+        breakLabel="..."
+        breakClassName="break-me"
+        pageCount={props.pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        disableInitialCallback={true}
+        initialPage={props.initialPage}
+        forcePage={props.initialPage}
+        containerClassName="pagination"
+        activeClassName="active"
+      />
     </>
   );
 };
