b67e4cc895d0b511f0aeb3a883de4913de19af77
Eseoghene Erigha <ese.erigha@gmail.com>
2021-03-10 02:25:46 +0100

    added more space to tmdb footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,16 @@ import tmdbLogo from 'assets/img/tmdb_logo.png';
 const Footer = () => (
   <footer className="footer text-center mt-4 mb-4 white">
     &#169; 2021 made with
-    <i className="fa fa-heart heart space-left-5" aria-hidden="true" />
-    <span className="space-left-5">by</span>
-    <a href="https://github.com/eseerigha/movie-finder" className="space-left-5">
+    <i className="fa fa-heart heart space-left-7" aria-hidden="true" />
+    <span className="space-left-7">by</span>
+    <a href="https://github.com/eseerigha/movie-finder" className="space-left-7">
       eseerigha
     </a>
     <a
       href="https://developers.themoviedb.org/3/getting-started/introduction"
       rel="noopener noreferrer"
       target="_blank"
-      className="space-left-5"
+      className="space-left-15"
     >
       <Image src={tmdbLogo} />
     </a>
