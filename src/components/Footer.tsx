b4853f11f17eb6d79830b1db80a80fe36f658000
Eseoghene Erigha <ese.erigha@gmail.com>
2021-03-10 02:19:08 +0100

    Make navbar responsive and movie tmdb logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import tmdbLogo from 'assets/img/tmdb_logo.png';
 
 const Footer = () => (
   <footer className="footer text-center mt-4 mb-4 white">
@@ -7,6 +9,14 @@ const Footer = () => (
     <span className="space-left-5">by</span>
     <a href="https://github.com/eseerigha/movie-finder" className="space-left-5">
       eseerigha
+    </a>
+    <a
+      href="https://developers.themoviedb.org/3/getting-started/introduction"
+      rel="noopener noreferrer"
+      target="_blank"
+      className="space-left-5"
+    >
+      <Image src={tmdbLogo} />
     </a>
   </footer>
 );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,47 +2,40 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import Search from 'components/Search';
-import tmdbLogo from 'assets/img/tmdb_logo.png';
 
 const NavBar = () => (
-  <Navbar fixed="top" bg="dark" variant="dark" expand="md" className="bg-black">
+  <Navbar fixed="top" bg="dark" variant="dark" expand="md" className="nav-menu bg-black">
     <Container>
       <Navbar.Brand href="/">MovieFlix</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Item>
-            <Nav.Link href="#features">Popular</Nav.Link>
+            <Nav.Link href="#features" className="filter">
+              Popular
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#pricing">Now playing</Nav.Link>
+            <Nav.Link href="#pricing" className="filter">
+              Now playing
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#pricing">Top rated</Nav.Link>
+            <Nav.Link href="#pricing" className="filter">
+              Top rated
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#pricing">Upcoming</Nav.Link>
+            <Nav.Link href="#pricing" className="filter">
+              Upcoming
+            </Nav.Link>
           </Nav.Item>
         </Nav>
         <Nav>
           <Nav.Item>
             <Nav.Link eventKey={2}>
               <Search />
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey={1} href="https://github.com/eseerigha/movie-finder">
-              <i className="fab fa-github fa-2x white mt-6" aria-hidden="true" />
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              eventKey={1}
-              href="https://developers.themoviedb.org/3/getting-started/introduction"
-            >
-              <Image src={tmdbLogo} />
             </Nav.Link>
           </Nav.Item>
         </Nav>
