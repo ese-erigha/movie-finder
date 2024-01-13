96fb051edfee37b3dec1235acfbba58b444657b6
Eseoghene Erigha <ese.erigha@gmail.com>
2024-01-13 01:27:07 -0500

    chore: fix service

--- src/api/movieService.ts
+++ src/api/movieService.ts
@@ -3,6 +3,9 @@ import { GenreResponse, ImageResponse, Movie, MoviesResponse, PersonnelResponse 
 import { axiosInstance } from 'api/httpClient';
 
 const MOVIE_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+const MOVIE_PATH = '/movie';
+const GENRE_LIST_PATH = `/genre${MOVIE_PATH}/list`
+
 export const MOVIE_DB_IMAGE_URL: {
   small: string;
   medium: string;
@@ -21,24 +24,24 @@ const fetchData = async <T>(path: string, config?: AxiosRequestConfig): Promise<
 };
 
 export const getMovies = (params: { category: string; page: number }) =>
-  fetchData<MoviesResponse>(`/movie/${params.category}`, { params: { page: params.page } });
+  fetchData<MoviesResponse>(`${MOVIE_PATH}/${params.category}`, { params: { page: params.page } });
 
 export const searchMovies = (params: { query: string; page: number }) =>
   fetchData<MoviesResponse>(`/search/movie`, {
     params: { query: params.query, page: params.page },
   });
 
-export const getGenres = () => fetchData<GenreResponse>('/genre/movie/list');
+export const getGenres = () => fetchData<GenreResponse>(GENRE_LIST_PATH);
 
-export const getMovie = (id: string) => fetchData<Movie>(`/movie/${id}`);
+export const getMovie = (id: string) => fetchData<Movie>(`${MOVIE_PATH}/${id}`);
 
-export const getActors = (id: string) => fetchData<PersonnelResponse>(`/movie/${id}/credits`);
+export const getActors = (id: string) => fetchData<PersonnelResponse>(`${MOVIE_PATH}/${id}/credits`);
 
 export const getMovieImages = (id: string) =>
-  fetchData<ImageResponse>(`/movie/${id}/images`, { params: { language: 'null' } });
+  fetchData<ImageResponse>(`${MOVIE_PATH}/${id}/images`, { params: { language: 'null' } });
 
 export const getRecommendations = (id: string) =>
-  fetchData<MoviesResponse>(`/movie/${id}/recommendations`, {
+  fetchData<MoviesResponse>(`${MOVIE_PATH}/${id}/recommendations`, {
     params: {
       language: 'null',
       page: 1,
